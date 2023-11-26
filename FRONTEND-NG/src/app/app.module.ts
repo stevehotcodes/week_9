46d3f2fc2733c325071a51e739ba2b6ea127cc65
annanodiyo <annanodiyo@gmail.com>
2023-11-26 14:04:35 +0300

    uer registration and login form

--- FRONTEND-NG/src/app/app.module.ts
+++ FRONTEND-NG/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
