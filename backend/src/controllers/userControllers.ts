5636b76f7f7cacfd30051844f3f6fe2b48f5066e
Stephen Ondieki <stevehotcodes@gmail.com>
2023-11-27 17:28:58 +0300

    worked on the functionality that admin can switch order status

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { registerUserSchema } from "../validators/validators";
 import Connection from "../dbhelpers/dbhelpers";
 import { isEmpty } from "lodash";
@@ -7,7 +7,16 @@ import bcrypt from "bcrypt";
 import mssql from "mssql";
 import { dbConfig } from "../config/dbConfig";
 import jwt from "jsonwebtoken";
-import { IUserDetails } from "../interfaces/userInterface";
+import { IUserDetails, User } from "../interfaces/userInterface";
+import { ExtendedUser } from "../middlewares/verifyTokens";
+import ejs from "ejs";
+import securePassword from'secure-random-password'
+
+
+const baseUrl="http://localhost:3000"
+
+const APPHOST = process.env.APPHOST || 'http://localhost'
+const PORT = process.env.PORT || 3000
 
 const dbhelper = new Connection();
 
@@ -108,3 +117,97 @@ export const getAUserById =async(req:Request,res:Response)=>{
     
   }
 }
+
+
+
+export const updateUser =async (req:Request,res:Response)=>{
+  try{
+      let{id}=req.params
+      let { firstname, lastname, email, password }=req.body
+      if(!firstname||!lastname ||!email||!password ){
+          return res.status(400).json({message: 'required values are missing kindly check again'})
+      }
+      let user:User= await (await dbhelper.execute('getAUserById', {id})).recordset[0]
+      if(!user){
+          return res.status(404).json({message: 'The user does not exist'});
+
+       }
+      const hashedPassword=await bcrypt.hash(password, 5);
+
+      await dbhelper.execute('updateUser',{id,firstname, lastname, email, password:hashedPassword})
+      return res.status(200).json({message:"the user's details was updated successfully "})
+
+  }
+  catch(error:any){
+          return res.status(500).json({message:error.message})
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// export const forgotPassword:RequestHandler = async (req, res) => {
+//   // sends an email to the user to reset password
+//   try {
+
+//       let link =  APPHOST + ':' + PORT + '/users/reset-password'
+//       const {email} = req.query as {email:string}
+//       if (!email) {
+//           return res.status(400).json({message: 'provide an email in the query'})
+//       }
+//       const user = await getUser('email', email)
+//       if (user) {
+//           const newPassword = securePassword.randomPassword({
+//               length:12,
+//               characters: [
+//                   securePassword.upper,
+//                   securePassword.lower,
+//                   securePassword.symbols,
+//                   securePassword.digits
+//               ]
+//           })
+
+//           const token = jwt.sign({id:user.id,password:newPassword}, process.env.SECRET_KEY as string, {expiresIn:'300s'})
+//           link = link+'?reset='+token
+//           await ejs.renderFile(path.resolve(__dirname, '../../templates/reset-password-email.ejs'), {firstName:user.firstname, newPassword, link}, async (err, emailHTML)=>{
+//               if (err) {
+//                   console.error(err)
+//                   return false
+//               }
+//               const mailer = NodeMail.getInstance()
+//               await mailer.send(user.email, 'Password Reset Link', emailHTML)
+//               return true
+//           })
+//       }
+//       return res.status(200).json({message: `If a user exists with email: <${email}> a reset link will be sent to the email provided. Check spam folder in case you don't see any email.`})
+//   }
+//   catch (error:any) {
+//       return res.status(500).json({message: error.message})
+//   }
+// }
+
+
+
+
+
+
+
+
+// export const resetPassword =async(req:ExtendedUser,res:Response)=>{
+//   try{
+
+//      const {reset}=req.query
+
+
+
+//   }
+//   catch(error:any){
+
+//   }
+// }
