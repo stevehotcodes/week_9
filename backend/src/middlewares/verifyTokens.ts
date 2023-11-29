3ad3a0a3dd0f8b55921b1e962efa34b8722c6979
Stephen Ondieki <stevehotcodes@gmail.com>
2023-11-29 12:00:22 +0300

    added the functionality of the navigating to user dashboard when the user logins

--- backend/src/middlewares/verifyTokens.ts
+++ backend/src/middlewares/verifyTokens.ts
@@ -17,7 +17,6 @@ export const verifyToken = (
 ) => {
   try {
     const token = req.headers["token"] as string;
-
     if (!token) {
       return res.status(401).json({
         message: "You do not have access",
@@ -30,6 +29,8 @@ export const verifyToken = (
     if(privilege&&decodedUserData.role !==privilege){
       return res.status(401).json({message:"Unauthorised"})
     }
+    
+  
   } catch (error:any) {
     return res.json({
       message: error.message,
